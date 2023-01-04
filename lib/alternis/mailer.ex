68be0924bf3caf35632b9a2c79d17e344da189b8
Leo Amigood <leo@amigood.com>
2023-01-04 23:02:02 +0100

    Setup for dialyzer and credo

--- lib/alternis/mailer.ex
+++ lib/alternis/mailer.ex
@@ -1,3 +1,5 @@
 defmodule Alternis.Mailer do
   use Swoosh.Mailer, otp_app: :alternis
+
+  @moduledoc false
 end
--- test/support/data_case.ex
+++ test/support/data_case.ex
@@ -16,6 +16,8 @@ defmodule Alternis.DataCase do
 
   use ExUnit.CaseTemplate
 
+  alias Ecto.Adapters.SQL.Sandbox
+
   using do
     quote do
       alias Alternis.Repo
@@ -36,8 +38,8 @@ defmodule Alternis.DataCase do
   Sets up the sandbox based on the test tags.
   """
   def setup_sandbox(tags) do
-    pid = Ecto.Adapters.SQL.Sandbox.start_owner!(Alternis.Repo, shared: not tags[:async])
-    on_exit(fn -> Ecto.Adapters.SQL.Sandbox.stop_owner(pid) end)
+    pid = Sandbox.start_owner!(Alternis.Repo, shared: not tags[:async])
+    on_exit(fn -> Sandbox.stop_owner(pid) end)
   end
 
   @doc """
